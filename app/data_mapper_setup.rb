a176253c7a627730d3185c25fd0ff6d317365893
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-06-30 10:52:38 +0100

    add env variables for different databases

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,8 +1,14 @@
-require 'data_mapper'
-require './app/models/link'
+env = ENV['RACK_ENV'] || 'development' # checks for current environment from
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager")
+require 'data_mapper' # get datamapper gem
 
-DataMapper.finalize
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager") # tells DataMapper where the databse is on your machine
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './app/models/link' # get application code that will use DM
+# require './app/link'
+
+DataMapper.finalize # finalizes models
 
 DataMapper.auto_upgrade!
