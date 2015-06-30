be46e08dfc4c08b57cc8755f4e53373c1d82a31f
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-30 18:34:46 +0100

    heroku config

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,11 +1,13 @@
-env = 'test' # checks for current environment from
-# env = ENV['RACK_ENV'] || 'development'
+env = ENV['RACK_ENV'] || 'development'
+
+# checks for current environment from
 
 require 'data_mapper' # get datamapper gem
+require 'dm-postgres-adapter'
 
 # DataMapper.setup(:default, "postgres://localhost/bookmark_manager") # tells DataMapper where the databse is on your machine
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmark_manager#{env}')
 
 require './app/models/link' # get application code that will use DM
 # require './app/link'
